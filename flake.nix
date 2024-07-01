9a1d7792dfb2e3335e195ccacb10d85fbcabc55a
John Lewis <github@jlewis.sh>
2024-06-30 22:14:32 -0500

    feat: bootstrapped rust env

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Provides basic Rust toolchain support.";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, rust-overlay, flake-utils }: 
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        toolchain = pkgs.rust-bin.selectLatestNightlyWith (toolchain: toolchain.default.override {
+          extensions = [ "rust-src" "rust-analyzer" ];
+        });
+      in {
+        devShell = pkgs.mkShell {
+          nativeBuildInputs = [
+            pkgs.pkg-config pkgs.openssl
+            toolchain
+          ];
+        };
+      });
+}
