e84419d8c04004353f77c01b79e4146dccf0a5fb
John Lewis <github@jlewis.sh>
2024-07-02 15:21:54 -0500

    feat: added a `read` method to `StorageClient`

--- crates/storage/src/lib.rs
+++ crates/storage/src/lib.rs
@@ -1,6 +1,7 @@
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 use serde::{Deserialize, Serialize};
+use tokio::io::AsyncRead;
 
 pub type DynStorageClient = Box<dyn StorageClient + Send + Sync + 'static>;
 
@@ -19,6 +20,30 @@ impl StorageCredentials {
 
 pub struct LocalTmpStorageClient(PathBuf);
 
-pub trait StorageClient {}
+#[derive(thiserror::Error, Debug)]
+pub enum ReadError {
+  #[error("the file was not available in storage: {0}")]
+  NotFound(PathBuf),
+  #[error("a local filesystem error occurred: {0}")]
+  IoError(#[from] std::io::Error),
+}
 
-impl StorageClient for LocalTmpStorageClient {}
+#[async_trait::async_trait]
+pub trait StorageClient {
+  async fn read(&self, path: &Path) -> Result<Box<dyn AsyncRead>, ReadError>;
+}
+
+#[async_trait::async_trait]
+impl StorageClient for LocalTmpStorageClient {
+  async fn read(&self, path: &Path) -> Result<Box<dyn AsyncRead>, ReadError> {
+    let path = self.0.as_path().join(path);
+
+    if !std::fs::exists(&path)? {
+      return Err(ReadError::NotFound(path));
+    }
+
+    let file = tokio::fs::File::open(&path).await?;
+
+    Ok(Box::new(file))
+  }
+}
