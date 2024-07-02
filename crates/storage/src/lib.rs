532c388c18b6e34c13e34c89a4c3b7e98925dd90
John Lewis <github@jlewis.sh>
2024-07-02 15:31:38 -0500

    chore: move `LocalStorageClient` to its own module

--- crates/storage/src/lib.rs
+++ crates/storage/src/lib.rs
@@ -1,7 +1,11 @@
+pub mod local;
+
 use std::path::{Path, PathBuf};
 
 use serde::{Deserialize, Serialize};
 use tokio::io::AsyncRead;
+
+use self::local::LocalStorageClient;
 
 pub type DynStorageClient = Box<dyn StorageClient + Send + Sync + 'static>;
 
@@ -13,12 +17,10 @@ pub enum StorageCredentials {
 impl StorageCredentials {
   pub async fn client(&self) -> DynStorageClient {
     match self {
-      Self::Local(path) => Box::new(LocalStorageClient(path.clone())),
+      Self::Local(path) => Box::new(LocalStorageClient::new(path.clone())),
     }
   }
 }
-
-pub struct LocalStorageClient(PathBuf);
 
 #[derive(thiserror::Error, Debug)]
 pub enum ReadError {
@@ -31,19 +33,4 @@ pub enum ReadError {
 #[async_trait::async_trait]
 pub trait StorageClient {
   async fn read(&self, path: &Path) -> Result<Box<dyn AsyncRead>, ReadError>;
-}
-
-#[async_trait::async_trait]
-impl StorageClient for LocalStorageClient {
-  async fn read(&self, path: &Path) -> Result<Box<dyn AsyncRead>, ReadError> {
-    let path = self.0.as_path().join(path);
-
-    if !std::fs::exists(&path)? {
-      return Err(ReadError::NotFound(path));
-    }
-
-    let file = tokio::fs::File::open(&path).await?;
-
-    Ok(Box::new(file))
-  }
 }
---
+++ crates/storage/src/local.rs
@@ -0,0 +1,27 @@
+use std::path::{Path, PathBuf};
+
+use tokio::io::AsyncRead;
+
+use super::{ReadError, StorageClient};
+
+pub struct LocalStorageClient(PathBuf);
+
+impl LocalStorageClient {
+  pub fn new(path: PathBuf) -> Self { Self(path) }
+}
+
+#[async_trait::async_trait]
+impl StorageClient for LocalStorageClient {
+  async fn read(&self, path: &Path) -> Result<Box<dyn AsyncRead>, ReadError> {
+    let path = self.0.as_path().join(path);
+
+    // make sure it exists
+    if !std::fs::exists(&path)? {
+      return Err(ReadError::NotFound(path));
+    }
+
+    let file = tokio::fs::File::open(&path).await?;
+
+    Ok(Box::new(file))
+  }
+}
