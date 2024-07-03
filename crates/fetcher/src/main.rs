3f646a9a985e34e42ac0c686d0e5e2384c3910bf
John Lewis <github@jlewis.sh>
2024-07-03 11:50:06 -0500

    feat: added simple axum app to `fetcher`

--- crates/fetcher/src/main.rs
+++ crates/fetcher/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-  println!("Hello, world!");
+use axum::{routing::get, Router};
+
+async fn hello_world_handler() -> &'static str { "Hello, World!" }
+
+#[tokio::main]
+async fn main() {
+  let app = Router::new().route("/", get(hello_world_handler));
+
+  let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+  axum::serve(listener, app).await.unwrap();
 }
