8863601946510597d8192d8be0f34421c149c196
John Lewis <github@jlewis.sh>
2024-07-05 11:58:48 -0500

    feat: add `core_types` and `db` crate

---
+++ crates/core_types/src/store.rs
@@ -0,0 +1,12 @@
+use serde::{Deserialize, Serialize};
+
+pub const STORE_TABLE_NAME: &str = "stores";
+
+#[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+#[cfg_attr(feature = "ssr", serde(from = "crate::ssr::UlidOrThing"))]
+pub struct StoreRecordId(pub ulid::Ulid);
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct Store {
+  pub id: StoreRecordId,
+}
