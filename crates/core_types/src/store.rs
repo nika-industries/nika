7b86a9cfc1b8a5019d6f930f1b64c8e80575d6fc
John Lewis <github@jlewis.sh>
2024-07-06 14:27:17 -0500

    fix: change db table `stores` to `store`

--- crates/core_types/src/store.rs
+++ crates/core_types/src/store.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-pub const STORE_TABLE_NAME: &str = "stores";
+pub const STORE_TABLE_NAME: &str = "store";
 
 #[derive(Serialize, Deserialize, Debug, Clone, Copy, PartialEq, Eq, Hash)]
 #[cfg_attr(feature = "ssr", serde(from = "crate::ssr::UlidOrThing"))]
