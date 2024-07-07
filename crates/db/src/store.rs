8e926c638fad4483493683ec891b8a3e401b24f6
John Lewis <github@jlewis.sh>
2024-07-07 11:49:02 -0500

    fix: add more docs to `fetch_store_by_name`

--- crates/db/src/store.rs
+++ crates/db/src/store.rs
@@ -5,6 +5,9 @@ use super::DbConnection;
 
 impl DbConnection {
   /// Fetches the [`core_types::Store`] matching the given name from the DB.
+  ///
+  /// The `store` table has a unique index on the `name` field, which is why the
+  /// return type is an `Option<>` instead of a `Vec<>`.
   pub async fn fetch_store_by_name(
     &self,
     name: &str,
