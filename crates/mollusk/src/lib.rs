0b3f2d14db5967664ad3aa5eb10ae523c3f7e974
John Lewis <github@jlewis.sh>
2024-07-08 14:44:37 -0500

    feat: prototyped `ApiError`

---
+++ crates/mollusk/src/lib.rs
@@ -0,0 +1,28 @@
+use axum::{http::StatusCode, response::IntoResponse};
+use miette::Diagnostic;
+
+pub trait ApiError: Diagnostic {
+  /// The [`StatusCode`] that the error should return.
+  fn status_code(&self) -> StatusCode;
+  /// The unique slug for the error, to enable client-side handling.
+  fn slug(&self) -> &'static str;
+  /// The human-readable description for the error.
+  fn description(&self) -> String;
+  /// This method should run any logging or tracing calls attached to the error.
+  fn tracing(&self);
+
+  fn into_response(&self) -> axum::response::Response {
+    self.tracing();
+    return (
+      self.status_code(),
+      serde_json::json!({
+        "error": {
+          "id": self.slug(),
+          "description": ApiError::description(self),
+        },
+      })
+      .to_string(),
+    )
+      .into_response();
+  }
+}
