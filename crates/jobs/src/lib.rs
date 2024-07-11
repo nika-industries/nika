0e478a3f7f533dce8f403edc6f38ea213aa4095f
John Lewis <github@jlewis.sh>
2024-07-11 13:00:17 -0500

    feat: added `HealthCheckJob` in `jobs`

--- crates/jobs/src/lib.rs
+++ crates/jobs/src/lib.rs
@@ -1,1 +1,13 @@
+use std::convert::Infallible;
 
+use apalis::prelude::Job;
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct HealthCheckJob;
+
+impl Job for HealthCheckJob {
+  const NAME: &'static str = "jobs::HealthCheckJob";
+}
+
+async fn execute_health_check_job() -> Result<bool, Infallible> { Ok(true) }
