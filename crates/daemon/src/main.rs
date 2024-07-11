d60608ea1a66b8edf7d96986533b47f0b0cc5065
John Lewis <github@jlewis.sh>
2024-07-11 13:00:01 -0500

    feat: bootstrapped `daemon` further

--- crates/daemon/src/main.rs
+++ crates/daemon/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-  println!("Hello, world!");
+#[tokio::main]
+async fn main() -> miette::Result<()> {
+  tracing_subscriber::fmt::init();
+
+  println!();
+  for line in art::ascii_art!("../../media/ascii_logo.png").lines() {
+    println!("{}", line);
+  }
+  println!();
+
+  Ok(())
 }
