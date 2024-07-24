29a6c29aef21ca9c75f9b4a2e9385915d70aef11
John Lewis <github@jlewis.sh>
2024-07-24 16:51:31 -0500

    feat: bootstrapped cli crate

---
+++ crates/cli/src/main.rs
@@ -0,0 +1,8 @@
+//! The client CLI for the Nika service.
+//!
+//! This is the main entry point for the CLI, and is responsible for parsing
+//! command line arguments and invoking the appropriate subcommand.
+
+fn main() {
+  println!("Hello, world!");
+}
