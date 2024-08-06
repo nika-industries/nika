c1e2856f6d1a3f9341805c601fea445b900d29da
John Lewis <github@jlewis.sh>
2024-08-06 14:40:39 -0500

    feat: added nar subcommands to CLI

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,10 +1,14 @@
 //! CLI for the Nika project.
 
-use clap::{Parser, Subcommand};
+mod nar;
+
+use std::path::PathBuf;
+
+use clap::{Args, Parser, Subcommand};
 
 /// CLI for the Nika project.
 #[derive(Parser, Debug)]
-struct Args {
+struct Cli {
   #[command(subcommand)]
   command: Command,
 }
@@ -19,12 +23,46 @@ enum Command {
 #[derive(Subcommand, Debug)]
 enum NarCommand {
   /// Create a new NAR archive.
-  Create,
+  Create(NarCreateArgs),
   /// Extract an existing NAR archive.
-  Extract,
+  Extract(NarExtractArgs),
+}
+
+#[derive(Args, Debug)]
+struct NarCreateArgs {
+  /// The file system object to archive.
+  target: PathBuf,
+  /// Sets the path of the output file.
+  #[arg(short, long)]
+  output: Option<PathBuf>,
+}
+
+#[derive(Args, Debug)]
+struct NarExtractArgs {
+  /// The NAR archive to extract.
+  target: PathBuf,
+  /// Sets the path of the output directory.
+  #[arg(short, long)]
+  output: Option<PathBuf>,
 }
 
 fn main() {
-  let args = Args::parse();
-  dbg!(args);
+  tracing_subscriber::fmt().without_time().init();
+
+  let args = Cli::parse();
+
+  match args.command {
+    Command::Nar(NarCommand::Create(args)) => {
+      let val = crate::nar::create_nar_archive(args);
+      if val.is_err() {
+        std::process::exit(1);
+      }
+    }
+    Command::Nar(NarCommand::Extract(args)) => {
+      let val = crate::nar::extract_nar_archive(args);
+      if val.is_err() {
+        std::process::exit(1);
+      }
+    }
+  }
 }
