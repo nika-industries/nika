e822e943b9f6a385129b193c04e3dff6a81b5c90
John Lewis <github@jlewis.sh>
2024-08-11 19:16:34 -0500

    fix: add serde traits to `Slug`

--- crates/core_types/src/slug.rs
+++ crates/core_types/src/slug.rs
@@ -2,6 +2,8 @@ pub use slug::slugify;
 
 #[nutype::nutype(
   sanitize(with = |s: String| slugify(&s)),
-  derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Hash),
+  derive(
+    Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash
+  ),
 )]
 pub struct Slug(String);
