0bc68d6c322d5a625c3631814babc834221e4cf5
John Lewis <github@jlewis.sh>
2024-08-12 10:31:31 -0500

    chore: remove `HealthCheck` task

--- crates/tasks/src/lib.rs
+++ crates/tasks/src/lib.rs
@@ -1,12 +1,10 @@
 //! Provides types and business logic for all platform tasks used with [`rope`].
 
 mod fetch_store_creds;
-mod health_check;
 mod naive_upload;
 
 pub use rope::Task;
 
 pub use self::{
-  fetch_store_creds::FetchStoreCredsTask, health_check::HealthCheckTask,
-  naive_upload::NaiveUploadTask,
+  fetch_store_creds::FetchStoreCredsTask, naive_upload::NaiveUploadTask,
 };
