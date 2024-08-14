c78f46155c4c82d792a9ae3668ed58132375a428
John Lewis <github@jlewis.sh>
2024-08-14 14:35:23 -0500

    feat: add `db` module to `tasks`

---
+++ crates/tasks/src/db/mod.rs
@@ -0,0 +1,34 @@
+use rope::Task;
+use serde::{Deserialize, Serialize};
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct FetchStoreByNameFromDbTask {
+  store_name: String,
+}
+
+impl FetchStoreByNameFromDbTask {
+  pub fn new(store_name: String) -> Self { Self { store_name } }
+}
+
+#[async_trait::async_trait]
+impl Task for FetchStoreByNameFromDbTask {
+  const NAME: &'static str = "FetchStoreByNameFromDb";
+
+  type Response = Option<core_types::Store>;
+  type Error = mollusk::InternalError;
+  type State = db::DbConnection;
+
+  async fn run(
+    self,
+    state: Self::State,
+  ) -> Result<Self::Response, Self::Error> {
+    state
+      .fetch_store_by_name(&self.store_name)
+      .await
+      .map_err(|e| {
+        let error = format!("failed to fetch store by name: {}", e);
+        tracing::error!("{}", &error);
+        mollusk::InternalError::SurrealDbQueryError(error)
+      })
+  }
+}
--- crates/tasks/src/lib.rs
+++ crates/tasks/src/lib.rs
@@ -1,9 +1,10 @@
 //! Provides types and business logic for all platform tasks used with [`rope`].
 
+mod db;
 mod fetch_store_creds;
 mod naive_upload;
 mod prepare_fetch_payload;
 
 pub use rope::Task;
 
-pub use self::{fetch_store_creds::*, naive_upload::*};
+pub use self::{db::*, fetch_store_creds::*, naive_upload::*};
