83e91925debf42441a6e7c51228f0eec358bd902
John Lewis <github@jlewis.sh>
2024-08-20 19:06:22 -0500

    fix: impl `Display` for `Slug`

--- crates/slugger/src/lib.rs
+++ crates/slugger/src/lib.rs
@@ -6,7 +6,8 @@ pub use slug::slugify;
 #[nutype::nutype(
   sanitize(with = |s: String| slugify(&s)),
   derive(
-    Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash, AsRef
+    Debug, Clone, Serialize, Deserialize, PartialEq, Eq, PartialOrd, Ord, Hash,
+    AsRef, Display
   ),
 )]
 pub struct Slug(String);
