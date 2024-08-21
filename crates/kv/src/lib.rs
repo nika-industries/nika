f8091f6f596ee2862fb6038e9962014e2799b73e
John Lewis <github@jlewis.sh>
2024-08-21 10:45:51 -0500

    chore: move redis module to separate file

--- crates/kv/src/lib.rs
+++ crates/kv/src/lib.rs
@@ -1,6 +1,8 @@
 //! A generic interface for key-value stores.
 
 pub mod key;
+#[cfg(feature = "redis")]
+pub mod redis;
 
 use std::future::Future;
 
@@ -34,63 +36,3 @@ pub trait KvPrimitive {
 
 /// Defines composite and first-party methods for key-value stores.
 pub trait KvExtension: KvPrimitive {}
-
-#[cfg(feature = "redis")]
-pub mod redis {
-  //! Contains a Redis implementation of the key-value interface.
-
-  use fred::prelude::*;
-  use futures::TryFutureExt;
-
-  use super::*;
-
-  /// Redis implementation of the key-value interface
-  pub struct Redis {
-    client:           RedisClient,
-    _connection_task: fred::types::ConnectHandle,
-  }
-
-  impl Redis {
-    /// Create a new Redis store.
-    pub async fn new(
-      config: RedisConfig,
-      perf: Option<PerformanceConfig>,
-      connection: Option<ConnectionConfig>,
-      policy: Option<ReconnectPolicy>,
-    ) -> Result<Self, RedisError> {
-      let client = RedisClient::new(config, perf, connection, policy);
-      let _connection_task = client.init().await?;
-      Ok(Self {
-        client,
-        _connection_task,
-      })
-    }
-  }
-
-  impl KvPrimitive for Redis {
-    type PlatformError = RedisError;
-
-    fn get(
-      &self,
-      key: &str,
-    ) -> impl Future<Output = KvResult<Option<String>>> + Send {
-      self
-        .client
-        .get(key)
-        .map_err(|e: RedisError| KvError::PlatformError(e.to_string()))
-    }
-
-    fn set(
-      &self,
-      key: &str,
-      value: String,
-    ) -> impl Future<Output = KvResult<()>> + Send {
-      self
-        .client
-        .set(key, value, None, None, false)
-        .map_err(|e: RedisError| KvError::PlatformError(e.to_string()))
-    }
-  }
-
-  impl KvExtension for Redis {}
-}
