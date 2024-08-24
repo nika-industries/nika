13ffcb003b37d0e090ee5097092b9c91d27b11c1
John Lewis <github@jlewis.sh>
2024-08-24 13:23:06 -0500

    feat: add `Model` trait

--- crates/models/src/lib.rs
+++ crates/models/src/lib.rs
@@ -7,8 +7,31 @@ mod store;
 mod token;
 mod user;
 
+use std::{fmt::Debug, hash::Hash};
+
+use serde::{de::DeserializeOwned, Serialize};
+use slugger::Slug;
 pub use ulid::Ulid;
 
 pub use self::{
   org::*, perms::*, storage_creds::*, store::*, token::*, user::*,
 };
+
+type SlugFieldGetter<T> = fn(&T) -> Slug;
+
+/// Represents a model in the database.
+pub trait Model:
+  Clone + Debug + PartialEq + Serialize + DeserializeOwned + 'static
+{
+  /// The model's ID type
+  type Id: Clone + Debug + PartialEq + Eq + Hash + Serialize + DeserializeOwned;
+  /// The table name in the database.
+  const TABLE_NAME: &'static str;
+
+  /// The model's indices, an array of tuples containing the index name and a
+  /// function that returns the index value.
+  const INDICES: &'static [(&'static str, SlugFieldGetter<Self>)];
+
+  /// Returns the model's ID.
+  fn id(&self) -> Self::Id;
+}
