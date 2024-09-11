f1362fa2d9a070442ea28f44c5893ba5aefc2762
John Lewis <github@jlewis.sh>
2024-09-11 12:02:14 -0500

    chore: add comments to rust-builds nix module

--- flake-modules/rust-builds/default.nix
+++ flake-modules/rust-builds/default.nix
@@ -2,6 +2,7 @@ localFlake: { ... }: {
   perSystem = { system, pkgs, ... }: let
     filter = localFlake.inputs.nix-filter.lib;
 
+    # configure the source
     src = filter {
       root = ../../.;
       include = [
@@ -12,6 +13,7 @@ localFlake: { ... }: {
       ];
     };
 
+    # build the CI and dev toolchains
     toolchain = p: p.rust-bin.selectLatestNightlyWith (toolchain: toolchain.minimal.override {
       extensions = [ "rustfmt" "clippy" ];
     });
@@ -20,8 +22,10 @@ localFlake: { ... }: {
       # targets = [ "wasm32-unknown-unknown" ];
     });
 
+    # configure crane to use the CI toolchain
     craneLib = (localFlake.inputs.crane.mkLib pkgs).overrideToolchain toolchain;
 
+    # arguments shared by all rust packages we build
     common-args = {
       inherit src;
       strictDeps = true;
@@ -38,14 +42,15 @@ localFlake: { ... }: {
       ];
     };
 
+    # build the deps for the whole workspace
     cargoArtifacts = craneLib.buildDepsOnly common-args;
 
+    # builder functions for individual crates    
     individual-crate-args = crate-name: common-args // {
       inherit cargoArtifacts;
       pname = crate-name;
       cargoExtraArgs = "-p ${crate-name}";
     };
-
     build-crate = name: craneLib.buildPackage (individual-crate-args name);
   in {
     packages = {
@@ -56,20 +61,25 @@ localFlake: { ... }: {
       dev-toolchain = dev-toolchain pkgs;
     };
     checks = {
+      # run clippy, denying warnings
       rust-cargo-clippy = craneLib.cargoClippy (common-args // {
         inherit cargoArtifacts;
         cargoClippyExtraArgs = "--all-targets -- --deny warnings";
       });
+      # run rust-doc, denying warnings
       rust-cargo-docs = craneLib.cargoDoc (common-args // {
         inherit cargoArtifacts;
         RUSTDOCFLAGS = "-D warnings";
       });
+      # run rust tests with nextest
       rust-cargo-nextest = craneLib.cargoNextest (common-args // {
         inherit cargoArtifacts;
         partitions = 1;
         partitionType = "count";
       });
+      # run cargo fmt, failing if not already formatted perfectly
       rust-cargo-fmt = craneLib.cargoFmt common-args;
+      # run cargo deny, failing if anything gets flagged
       rust-cargo-deny = craneLib.cargoDeny common-args;
     };
   };
