32c92529781b3b5d94898bebc76ebb32e1d786c7
John Lewis <github@jlewis.sh>
2024-09-17 14:44:35 -0500

    fix: repair tikv hashes

--- flake-modules/tikv/makeTikvBinary.nix
+++ flake-modules/tikv/makeTikvBinary.nix
@@ -7,9 +7,14 @@
   arch = { x86_64-linux = "amd64"; aarch64-linux = "arm64"; }.${system};
   url = "https://download.pingcap.org/tidb-community-server-v${version}-linux-${arch}.tar.gz";
 
+  hashes = {
+    x86_64-linux = "sha256-ZtFqm4PllBRIGiRLzBynWvdcmegXD8WMPzknXwJYKBg=";
+    aarch64-linux = "sha256-CovqGP4nciRWfB+mGQcCP+VBkVOmC6hzRXO2gvXylpc=";
+  };
+
   full-archive = pkgs.fetchzip {
     url = url;
-    hash = "sha256-ZtFqm4PllBRIGiRLzBynWvdcmegXD8WMPzknXwJYKBg=";
+    hash = hashes.${system};
   };
 in pkgs.stdenv.mkDerivation {
   inherit pname version;
