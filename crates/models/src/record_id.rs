9cff92fefe1badb832f8c1c3c671418cb70f3fd6
John Lewis <github@jlewis.sh>
2024-09-20 18:18:29 -0500

    fix(models): minor docs fix

--- crates/models/src/record_id.rs
+++ crates/models/src/record_id.rs
@@ -7,7 +7,7 @@ pub use ulid::Ulid;
 // derive because of the `PhantomData` field; the derives assume that the `T`
 // generic also has to implement the trait we're deriving.
 
-/// A record ID for a [`Model`] implementer.
+/// A record ID for a [`Model`](crate::Model) implementer.
 #[derive(Serialize, Deserialize)]
 pub struct RecordId<T> {
   id: Ulid,
