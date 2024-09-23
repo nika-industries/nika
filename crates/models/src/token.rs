e60e5688c30a3b9b796e295e17bae01ef1bc3bba
John Lewis <github@jlewis.sh>
2024-09-23 13:07:31 -0500

    feat(prime-domain+models): add token verification and authorization

--- crates/models/src/token.rs
+++ crates/models/src/token.rs
@@ -36,6 +36,13 @@ impl Model for Token {
   fn id(&self) -> TokenRecordId { self.id }
 }
 
+impl Token {
+  /// Check if the token has the given permissions.
+  pub fn authorized(&self, perms: &PermissionSet) -> bool {
+    self.perms.contains_set(perms)
+  }
+}
+
 /// A token create request.
 #[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct TokenCreateRequest {
@@ -61,46 +68,5 @@ impl From<TokenCreateRequest> for Token {
       owner:    input.owner,
       org:      input.org,
     }
-  }
-}
-
-/// Validates a token secret. Returns `true` if the secret is valid.
-pub fn validate_token_secret(input: impl AsRef<str>) -> bool {
-  let secret = input.as_ref();
-  if secret.len() != 64 {
-    return false;
-  }
-  secret
-    .chars()
-    .all(|c| c.is_ascii_alphanumeric() || c == '-')
-}
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-
-  #[test]
-  fn test_valid_secret() {
-    let secret =
-      "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6-7-8-9-abcde";
-    assert!(validate_token_secret(secret));
-  }
-
-  #[test]
-  fn test_invalid_secret_length() {
-    let secret = "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6-7-8-";
-    assert!(!validate_token_secret(secret));
-  }
-
-  #[test]
-  fn test_invalid_secret_characters() {
-    let secret = "a1b2c3d4e5f6g7h8i9j0k1l2m3n4o5p6q7r8s9t0u1v2w3x4y5z6-7-8-@";
-    assert!(!validate_token_secret(secret));
-  }
-
-  #[test]
-  fn test_empty_secret() {
-    let secret = "";
-    assert!(!validate_token_secret(secret));
   }
 }
--- crates/prime-domain/src/token.rs
+++ crates/prime-domain/src/token.rs
@@ -1,25 +1,83 @@
+use std::{future::Future, str::FromStr};
+
 pub use models::Token;
-use repos::TokenRepository;
+use repos::{FetchModelError, TokenRepository};
+
+/// The error type for token verification.
+#[derive(Debug, thiserror::Error, miette::Diagnostic)]
+pub enum TokenVerifyError {
+  /// The token ID is invalid.
+  #[error("invalid token ID")]
+  InvalidId,
+  /// The token ID was not found.
+  #[error("token ID not found")]
+  IdNotFound,
+  /// The token secret is invalid.
+  #[error("invalid token secret")]
+  InvalidSecret,
+  /// The token secret does not match the expected secret.
+  #[error("token secret mismatch")]
+  SecretMismatch,
+  /// An error occurred while fetching the token.
+  #[error("error fetching token")]
+  #[diagnostic_source]
+  FetchError(FetchModelError),
+}
 
 /// The definition for the [`Token`] domain model service.
-pub trait TokenService: Clone + Send + Sync + 'static {}
+pub trait TokenService: Clone + Send + Sync + 'static {
+  /// Verifies that the supplied token ID and secret are valid and exist.
+  fn verify_token_id_and_secret(
+    &self,
+    token_id: String,
+    token_secret: String,
+  ) -> impl Future<Output = Result<Token, TokenVerifyError>>;
+}
 
 /// Canonical service for the [`Token`] domain model.
 pub struct TokenServiceCanonical<R: TokenRepository> {
-  cache_repo: R,
+  token_repo: R,
 }
 
 impl<R: TokenRepository> Clone for TokenServiceCanonical<R> {
   fn clone(&self) -> Self {
     Self {
-      cache_repo: self.cache_repo.clone(),
+      token_repo: self.token_repo.clone(),
     }
   }
 }
 
 impl<R: TokenRepository> TokenServiceCanonical<R> {
   /// Create a new instance of the canonical [`Token`] service.
-  pub fn new(cache_repo: R) -> Self { Self { cache_repo } }
+  pub fn new(token_repo: R) -> Self { Self { token_repo } }
 }
 
-impl<R: TokenRepository> TokenService for TokenServiceCanonical<R> {}
+impl<R: TokenRepository> TokenService for TokenServiceCanonical<R> {
+  async fn verify_token_id_and_secret(
+    &self,
+    token_id: String,
+    token_secret: String,
+  ) -> Result<Token, TokenVerifyError> {
+    let token_id = models::TokenRecordId::from_str(&token_id)
+      .map_err(|_| TokenVerifyError::InvalidId)?;
+
+    let token = self
+      .token_repo
+      .fetch_model_by_id(token_id)
+      .await
+      .map_err(TokenVerifyError::FetchError)?;
+
+    let token = token.ok_or(TokenVerifyError::IdNotFound)?;
+
+    let token_secret_slug = models::StrictSlug::new(token_secret.clone());
+    if token_secret_slug.clone().into_inner() != token_secret {
+      return Err(TokenVerifyError::InvalidSecret);
+    }
+
+    if token.secret != token_secret_slug {
+      return Err(TokenVerifyError::SecretMismatch);
+    }
+
+    Ok(token)
+  }
+}
