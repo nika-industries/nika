0eed7068c1512524b4d8db8d51d2a0395d3cbad0
John Lewis <github@jlewis.sh>
2024-10-08 16:42:45 +0200

    feat(flake): add positionals to devshell bin commands

--- flake-modules/devshell/bin-commands.nix
+++ flake-modules/devshell/bin-commands.nix
@@ -3,19 +3,19 @@
   perBinaryCommands = binary: [
     {
       name = binary;
-      command = "cargo run --bin ${binary}";
+      command = "cargo run --bin ${binary} -- $@";
       help = "Run the `${bin-hl binary}` binary";
       category = "[local binary actions]";
     }
     {
       name = "${binary}-release";
-      command = "cargo run --release --bin ${binary}";
+      command = "cargo run --release --bin ${binary} -- $@";
       help = "Run the `${bin-hl binary}` binary in release mode";
       category = "[local binary actions]";
     }
     {
       name = "${binary}-watch";
-      command = "bacon -j run -- --bin ${binary}";
+      command = "bacon -j run -- --bin ${binary} -- $@";
       help = "Watch for changes and run the `${bin-hl binary}` binary";
       category = "[local binary actions]";
     }
