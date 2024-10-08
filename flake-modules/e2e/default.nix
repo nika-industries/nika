0de053353b90eaa55d7dacf73d0e096c576c3f28
John Lewis <github@jlewis.sh>
2024-10-08 15:36:57 +0200

    chore(flake): rearranged some test things

--- flake-modules/e2e/default.nix
+++ flake-modules/e2e/default.nix
@@ -1,16 +1,20 @@
 localFlake: { self, ... }: {
   perSystem = { pkgs, ... } @ perSystemTop: let
+    # prefix for test checks
+    test-prefix = "nixvm-test-";
+
     # the common module
     common = import ./common.nix { inherit pkgs; };
     # the args we'll give to each module
     module-args = perSystemTop // { inherit self common; };
     # helper to call a module with the args
     call = source: import source module-args;
-    # renames attrset keys and adds prefix
-    test-renamer = name: value: pkgs.lib.attrsets.nameValuePair "nixvm-test-${name}" value;
+    # renames attrset keys and adds test check prefix
+    test-renamer = name: value: pkgs.lib.attrsets.nameValuePair "${test-prefix}${name}" value;
     # helper to call a test module
     callTestModule = source: pkgs.lib.attrsets.mapAttrs' test-renamer (call source);
 
+    # modules here
     tikv-cluster-connect = callTestModule ./tikv-cluster-connect.nix;
   in {
     checks = { } // tikv-cluster-connect;
