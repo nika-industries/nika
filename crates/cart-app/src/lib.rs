91a3d54f4e03696451ac5a903d029ccc0d3c3f2c
John Lewis <github@jlewis.sh>
2024-10-15 16:40:06 +0200

    feat(cart): more cart skeleton

--- crates/cart-app/src/lib.rs
+++ crates/cart-app/src/lib.rs
@@ -35,44 +35,47 @@ pub fn App() -> impl IntoView {
   provide_meta_context();
 
   view! {
-    // injects a stylesheet into the document <head>
-    // id=leptos means cargo-leptos will hot-reload this stylesheet
     <Stylesheet id="leptos" href="/pkg/cart.css"/>
     <Style>{include_str!("../style/fonts.css")}</Style>
     <Link rel="preload" href="/fonts/inter.ttf" as_="font" type_="font/ttf" crossorigin="anonymous" />
 
-    // sets the document title
     <Title text="Welcome to Leptos"/>
 
-    // content for this welcome page
-    <Router>
-      <main>
-        <Routes fallback=|| "Page not found.".into_view()>
-          <Route path=StaticSegment("") view=HomePage/>
-        </Routes>
-      </main>
-    </Router>
+    <div class="h-screen flex justify-items-start items-start gap-4">
+      <div class="flex-none self-stretch bg-backgroundSecondary w-60 p-6 pt-8 flex flex-col gap-1 border-r border-gray-6">
+        <a href="/" class="link text-2xl font-semibold tracking-tight">"Cartographer"</a>
+        <div class="flex flex-col gap-1 pl-2">
+          <p class="text-content2">"Models"</p>
+          <div class="flex flex-col gap-1 pl-2">
+            <a href="/model/cache" class="link">"Cache"</a>
+            <a href="/model/entry" class="link">"Entry"</a>
+            <a href="/model/store" class="link">"Store"</a>
+            <a href="/model/token" class="link">"Token"</a>
+            // <a href="/model/org" class="link">"Org"</a>
+            // <a href="/model/user" class="link">"User"</a>
+          </div>
+        </div>
+      </div>
+      <div class="container py-8">
+        <Router>
+          <main>
+            <Routes fallback=|| "Page not found.".into_view()>
+              <Route path=StaticSegment("") view=HomePage/>
+            </Routes>
+          </main>
+        </Router>
+      </div>
+    </div>
   }
 }
 
 /// Renders the home page of your application.
-#[island]
+#[component]
 fn HomePage() -> impl IntoView {
-  // Creates a reactive value to update the button
-  let count = RwSignal::new(0);
-  let on_click = move |_| *count.write() += 1;
-
   view! {
-    <div class="h-screen flex justify-items-start items-start gap-4">
-      <div class="flex-none self-stretch w-60 bg-backgroundSecondary">
-
-      </div>
-      <div class="container py-8">
-        <div class="flex flex-col gap-4 items-start">
-          <p class="text-2xl font-semibold tracking-tight">"Welcome to Leptos!"</p>
-          <button class="btn" on:click=on_click>"Click Me: " {count}</button>
-        </div>
-      </div>
+    <div class="flex flex-col gap-4">
+      <p class="text-4xl font-semibold tracking-tight">"Welcome to Cartographer"</p>
+      <p class="text-lg text-content2">"Choose a page on the left to start exploring data."</p>
     </div>
   }
 }
