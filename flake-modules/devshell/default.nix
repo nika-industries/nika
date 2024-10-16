f8dc502504ec0a09b4832c3f8f0d2b82662de737
John Lewis <github@jlewis.sh>
2024-10-16 11:11:31 +0200

    fix(flake): correct `update-crate-graph` command again

--- flake-modules/devshell/default.nix
+++ flake-modules/devshell/default.nix
@@ -76,7 +76,7 @@ localFlake: { ... }: {
 
         {
           name = "update-crate-graph";
-          command = "cp $(nix build .#crate-graph-image --print-out-paths)/crate-graph.png $PRJ_ROOT/media/crate-graph.png -j";
+          command = "cp $(nix build .#crate-graph-image --print-out-paths)/crate-graph.png $PRJ_ROOT/media/crate-graph.png --no-preserve=mode";
           help = "Update the crate graph";
           category = "[repo actions]";
         }
