42aa3b346a4ca0fa4d51dc3a8b8941b7e0b72010
John Lewis <github@jlewis.sh>
2024-10-21 16:23:14 +0200

    feat(names): module docs

--- crates/names/src/lib.rs
+++ crates/names/src/lib.rs
@@ -1,4 +1,10 @@
 //! Generates names.
+//!
+//! Really, this crate is delightfully simple. I stole all the names from
+//! Zellij. I'm not even sorry; they're great names. (Zellij is MIT licensed,
+//! so I'm allowed to do that.)
+//!
+//! This is mainly used to generate random worker names at runtime.
 
 const ADJECTIVES: &[&str] = &[
   "adamant",
@@ -138,16 +144,16 @@ const NOUNS: &[&str] = &[
 
 use nanorand::Rng;
 
-fn pick_from(options: &[&'static str]) -> String {
+fn pick_from(options: &[&'static str]) -> &'static str {
   let mut rng = nanorand::tls_rng();
   let index = rng.generate::<usize>() % options.len();
-  options[index].to_string()
+  options[index]
 }
 
 /// Picks a random noun from the list.
-pub fn noun() -> String { pick_from(NOUNS) }
+pub fn noun() -> &'static str { pick_from(NOUNS) }
 /// Picks a random adjective from the list.
-pub fn adjective() -> String { pick_from(ADJECTIVES) }
+pub fn adjective() -> &'static str { pick_from(ADJECTIVES) }
 
 /// Assembles a name from a random noun and adjective.
 pub fn name() -> String {
