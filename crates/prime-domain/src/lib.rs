114ff278dacde3476e0bba84c410178ec45a1bb7
John Lewis <github@jlewis.sh>
2024-10-21 16:35:30 +0200

    feat(prime-domain): module docs

--- crates/prime-domain/src/lib.rs
+++ crates/prime-domain/src/lib.rs
@@ -1,5 +1,13 @@
 //! Provides prime-domain Services, the entry points for domain-specific
 //! business logic.
+//!
+//! This is where our services are defined. A service is a domain-specific
+//! business logic entrypoint. It has a single responsibility and has everything
+//! necessary to manipulate a sub-category of the domain.
+//!
+//! All of the business logic for a given domain operation should be inside a
+//! service method. Data should be validated and encapsulated before it gets to
+//! the service.
 
 mod cache;
 mod entry;
