bf342d6ea623f675fdcf8b31bbc0a82df9a0f41c
John Lewis <github@jlewis.sh>
2024-10-21 16:25:32 +0200

    feat(nasty): module docs

--- crates/nasty/src/lib.rs
+++ crates/nasty/src/lib.rs
@@ -1,4 +1,9 @@
 //! Utilities for interacting directly with Nix & Co.
+//!
+//! This crate is kind of half-baked, but it's generally what we will use for
+//! interacting directly with Nix primitives, the Nix daemon, and the Nix store.
+//!
+//! Expect more docs when this crate is more mature.
 
 #[cfg(feature = "nar")]
 pub mod nar;
