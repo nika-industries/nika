6745585dfeefdce9419379aeecebe4ab8583d31b
John Lewis <github@jlewis.sh>
2024-10-21 14:43:26 +0200

    feat(daemon): module docs

--- crates/daemon/src/main.rs
+++ crates/daemon/src/main.rs
@@ -1,4 +1,11 @@
 //! Binary for consuming and running tasks.
+//!
+//! This is useless and disabled for the moment because we have no long-running
+//! tasks, but we will use it when we do.
+//!
+//! It builds up the same app state as the API binary, and then spawns tokio
+//! tasks for each type of task that we want to listen for. It then executes
+//! each task that lands in the redis queue using its state.
 
 // use rope::Backend;
 
