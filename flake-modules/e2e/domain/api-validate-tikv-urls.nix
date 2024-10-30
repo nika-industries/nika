8c26e20bbccd6c08a8af0ec14457602c41d44c77
John Lewis <github@jlewis.sh>
2024-10-30 12:19:16 +0100

    feat(nix-tests): refurbish `validate-tikv-urls` test

--- flake-modules/e2e/domain/api-validate-tikv-urls.nix
+++ flake-modules/e2e/domain/api-validate-tikv-urls.nix
@@ -1,32 +1,65 @@
-{ pkgs, config, common, ... }: {
-  domain-api-validate-tikv-urls = pkgs.testers.runNixOSTest {
-    name = "domain-api-validate-tikv-urls";
+{ pkgs, config, common, self, ... }: let
+  api_configure = tikv-urls: {
+    imports = [ (common.assign-static 5) self.nixosModules.api ];
+    services.api = {
+      enable = true;
+      mockTempStorage = true;
+      tikvUrls = tikv-urls;
+    };
+    environment.systemPackages = with pkgs; [ curl jq ];
+  };
+in {
+
+  domain-api-reject-empty-tikv-urls = pkgs.testers.runNixOSTest {
+    name = "domain-api-reject-empty-tikv-urls";
+
+    nodes = { api = api_configure [ ]; };
+
+    testScript = ''
+      api.wait_for_unit("api.service")
+      # make sure "overall_status" has a "Down" key in it
+      api.wait_until_succeeds("curl http://localhost:3000/health | jq -e '.[\"overall_status\"] | has(\"Down\")'")
+    '';
+  };
+
+  domain-api-reject-blank-tikv-urls = pkgs.testers.runNixOSTest {
+    name = "domain-api-reject-blank-tikv-urls";
+
+    nodes = { api = api_configure [ "" ]; };
+
+    testScript = ''
+      api.wait_for_unit("api.service")
+      # make sure "overall_status" has a "Down" key in it
+      api.wait_until_succeeds("curl http://localhost:3000/health | jq -e '.[\"overall_status\"] | has(\"Down\")'")
+    '';
+  };
+
+  domain-api-reject-invalid-tikv-urls = pkgs.testers.runNixOSTest {
+    name = "domain-api-reject-invalid-tikv-urls";
+
+    nodes = { api = api_configure [ "localhost:2379" ]; };
+
+    testScript = ''
+      api.wait_for_unit("api.service")
+      # make sure "overall_status" has a "Down" key in it
+      api.wait_until_succeeds("curl http://localhost:3000/health | jq -e '.[\"overall_status\"] | has(\"Down\")'")
+    '';
+  };
+
+  domain-api-accept-valid-tikv-urls = pkgs.testers.runNixOSTest {
+    name = "domain-api-accept-valid-tikv-urls";
 
     nodes = {
-      api = {
-        imports = [ (common.assign-static 5) ];
-        environment.systemPackages = [ config.packages.api ];
-      };
+      api = api_configure [ "10.0.0.11:2379" ];
     } // common.tikv-basic;
 
     testScript = ''
-      pd1.wait_for_unit("network-online.target")
       pd1.wait_for_unit("pd.service")
-      tikv1.wait_for_unit("network-online.target")
       tikv1.wait_for_unit("tikv.service")
-    
-      api.wait_for_unit("network-online.target")
-
-      command = "api --mock-temp-storage health"
-      # API bin fails without the TIKV_URLS env var
-      api.fail(command)
-      # API bin fails with an empty TIKV_URLS env var
-      api.fail("TIKV_URLS=\'\' " + command)
-      # API bin fails with an unreachable URL in the TIKV_URLS env var
-      api.fail("TIKV_URLS=localhost:1234 " + command)
-
-      # API bin succeeds with a reachable URL in the TIKV_URLS env var
-      api.succeed("TIKV_URLS=10.0.0.11:2379 " + command)
+      api.wait_for_unit("api.service")
+      # make sure "overall_status" has a "Up" key in it
+      api.wait_until_succeeds("curl http://localhost:3000/health | jq -e '.[\"overall_status\"] == \"Ok\"'")
     '';
   };
+
 }
