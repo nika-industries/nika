a40896e22342f4c810557ad96703d2d2904cb0dd
John Lewis <github@jlewis.sh>
2024-11-05 14:52:47 +0100

    feat(stream-tools): drafted basic `CompAwareStream`

--- crates/stream-tools/src/lib.rs
+++ crates/stream-tools/src/lib.rs
@@ -3,11 +3,80 @@
 mod counted_async_reader;
 pub mod read_jump;
 
+use std::{
+  io::Read,
+  pin::Pin,
+  task::{Context, Poll},
+};
+
 pub use tokio::io::AsyncRead;
 
 pub use self::{
   counted_async_reader::CountedAsyncReader, read_jump::wrap_with_read_jump,
 };
 
-/// Trait alias for `Box<dyn AsyncReader + ...>`
-pub type DynAsyncReader = Box<dyn AsyncRead + Send + Unpin + 'static>;
+/// A stream that is aware of the compression algorithm used.
+pub struct CompAwareStream {
+  stream:    Box<dyn AsyncRead + Send + Unpin + 'static>,
+  algorithm: Option<dvf::CompressionAlgorithm>,
+}
+
+impl CompAwareStream {
+  /// Create a new `CompAwareStream`.
+  pub fn new(
+    stream: Box<dyn AsyncRead + Send + Unpin + 'static>,
+    algorithm: Option<dvf::CompressionAlgorithm>,
+  ) -> Self {
+    Self { stream, algorithm }
+  }
+
+  /// Get the compression algorithm used.
+  pub fn algorithm(&self) -> Option<dvf::CompressionAlgorithm> {
+    self.algorithm
+  }
+
+  /// Get the inner stream and the compression algorithm used.
+  pub fn into_inner(
+    self,
+  ) -> (
+    Box<dyn AsyncRead + Send + Unpin + 'static>,
+    Option<dvf::CompressionAlgorithm>,
+  ) {
+    (self.stream, self.algorithm)
+  }
+
+  /// Apply an adapter function to the inner stream.
+  pub fn map_stream<F>(self, f: F) -> Self
+  where
+    F: FnOnce(
+      Box<dyn AsyncRead + Send + Unpin + 'static>,
+    ) -> Box<dyn AsyncRead + Send + Unpin + 'static>,
+  {
+    Self {
+      stream:    f(self.stream),
+      algorithm: self.algorithm,
+    }
+  }
+
+  /// Apply a [`Read`] adapter function to the inner stream.
+  pub fn map_stream_read_adapted<F>(self, f: F) -> Self
+  where
+    F: FnOnce(
+      Box<dyn Read + Send + Unpin + 'static>,
+    ) -> Box<dyn Read + Send + Unpin + 'static>,
+  {
+    self.map_stream(|s| Box::new(wrap_with_read_jump(s, f)))
+  }
+}
+
+impl AsyncRead for CompAwareStream {
+  fn poll_read(
+    self: Pin<&mut Self>,
+    cx: &mut Context<'_>,
+    buf: &mut tokio::io::ReadBuf<'_>,
+  ) -> Poll<tokio::io::Result<()>> {
+    let this = Pin::into_inner(self);
+    let pinned = std::pin::pin!(&mut this.stream);
+    pinned.poll_read(cx, buf)
+  }
+}
